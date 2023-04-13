ffa97aa0fee1934e2d581936ab1f6ed8ba56cb21
Alexis Brian Llerena Saavedra <alexisllerenasaa@gmail.com>
2023-04-13 12:57:44 -0300

    cambiamos la variable de entorno para que lo decida azure

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express=require('express');
+require('dotenv').config()
 
 const app=express();
 
@@ -8,6 +9,8 @@ app.get('/',(req,res)=>{
 
 })
 
-app.listen(4000,()=>{
-    console.log("Servidor escuchando en el puerto 4000")
+const PORT=process.env.PORT 
+
+app.listen(PORT,()=>{
+    console.log(`SERVIDOR ESCUCHANDO EN EL PUERTO ${PORT}`)
 })
